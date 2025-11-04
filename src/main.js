882e305a46aeefcb593b3d04bbd6595f7b1cc43f
KOPP <koppkvn@gmail.com>
2025-11-04 14:50:03 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1468,7 +1468,7 @@ function initTreeDiagram() {
             scaleX: window.innerWidth <= 767 ? 1 : 0,
             scaleY: window.innerWidth <= 767 ? 0 : 1,
             transformOrigin: window.innerWidth <= 767 ? "top" : "left",
-            duration: 1.5,
+            duration: 3,
             onComplete: function () {
                 scrambleChildLabelCompareTwo.play(0);
                 gsap.set(".label.color.is--compare", {
